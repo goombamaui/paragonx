3c30207d14b3529346a2945ff4954cb4b23a2a73
Harry Chen <harrychen0314@gmail.com>
2022-09-20 21:02:47 -0700

    add instructor name
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -7,18 +7,21 @@
       <div class="title font-weight-bold text-h6">
         {{ event.title }}
       </div>
-      <div class="my-1 text-body-2">
+      <div class="mt-2 mb-1 text-body-2">
         {{ event.description }}
       </div>
       <div class="mt-3">
-        <v-chip v-if="event.dateEnd" class="mr-1 mb-2">
+        <v-chip v-if="event.dateEnd" class="mr-1 mb-2" small>
           {{ event.dateStart | formatDate }} to {{ event.dateEnd | formatDate }}
         </v-chip>
-        <v-chip v-else class="mr-1 mb-2">
+        <v-chip v-else class="mr-1 mb-2" small>
           {{ event.dateStart | formatDate }}
         </v-chip>
-        <v-chip class="mr-1 mb-2">
+        <v-chip small class="mr-1 mb-2">
           {{ event.time }}
+        </v-chip>
+        <v-chip v-if="event.instructor" small class="mr-1 mb-2">
+          {{ event.instructor.replace(/ *\([^)]*\) */g, "") }}
         </v-chip>
       </div>
     </div>
